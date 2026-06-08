afa90cacd49679e6bf6bfd0e9ffa3ff638d032d6
Satvik <sracharla014@gmail.com>
2026-06-08 19:44:29 -0400

    config changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/maa-village-2/",
+  base: '/maa-village-2',
   plugins: [react(), tailwindcss()],
 })
