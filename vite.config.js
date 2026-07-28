02049440b2e6ca5a3c3e3e767c64f1915d1ff7b5
Satvik <sracharla014@gmail.com>
2026-07-27 21:32:22 -0400

    edited config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/maa-village-2',
+  base: process.VITE_BASE_PATH || '/maa-village-3',
   plugins: [react(), tailwindcss()],
 })
